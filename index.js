ad5331c4d1876de2f5605eafdd9a79fa602e5c28
Gurleen Sethi <sarusethi@rocketmail.com>
2020-01-26 15:44:14 -0500

    Install Flutter.

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 const core = require("@actions/core");
 const github = require("@actions/github");
+const io = require("@actions/io");
+const toolCache = require("@actions/tool-cache");
+const exec = require("@actions/exec");
 
-try {
-  const nameToGreet = core.getInput("who-to-greet");
-  console.log(`Hello ${nameToGreet}!`);
-  const time = new Date().toTimeString();
-  core.setOutput("time", time);
-  const payload = JSON.stringify(github.context.payload, undefined, 2);
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+// try {
+//   const nameToGreet = core.getInput("who-to-greet");
+//   console.log(`Hello ${nameToGreet}!`);
+//   const time = new Date().toTimeString();
+//   core.setOutput("time", time);
+//   const payload = JSON.stringify(github.context.payload, undefined, 2);
+//   console.log(`The event payload: ${payload}`);
+// } catch (error) {
+//   core.setFailed(error.message);
+// }
+
+function getFlutterUrl(
+  options = { version: "v1.12.13+hotfix.5", channel: "stable" }
+) {
+  const version = options.version || "v1.12.13+hotfix.5";
+  const channel = options.channel || "stable";
+  return `https://storage.googleapis.com/flutter_infra/releases/${channel}/linux/flutter_linux_v${version}-${channel}.tar.xz`;
 }
+
+async function downloadFlutter() {
+  // Create a folder
+  let cachedPath = toolCache.find("flutter");
+  if (!!flutterPath) {
+    await io.mkdirP("flutter_sdk");
+    const url = getFlutterUrl();
+    const sdkFile = await toolCache.downloadTool(getFlutterUrl());
+    await toolCache.extractTar("flutter_sdk", sdkFile, "xz");
+    const sdkDir = "flutter_sdk/flutter";
+    cachedPath = toolCache.cacheDir(sdkDir);
+  }
+  core.exportVariable("FLUTTER_HOME", cachedPath);
+  core.addPath(cachedPath);
+}
+
+(async () => {
+  try {
+    await downloadFlutter();
+    exec.exec("flutter --version");
+  } catch (error) {
+    core.setFailed(error);
+  }
+})();
